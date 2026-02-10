1a0f0f39c0f4b3e3669a28047f8ef84dddc81889
DocSebas7 <81327636+itachi773@users.noreply.github.com>
2026-02-09 23:55:19 -0500

    Update owner number in config.js

---
+++ config.js
@@ -0,0 +1,37 @@
+// ======================
+// CONFIGURACIÓN GENERAL
+// ======================
+
+global.owner = [
+  ['573116414414', 'DocSebas', true] // cambia por tu número
+]
+
+global.mods = []
+global.prems = []
+
+global.prefix = '.'
+
+// ======================
+// INFO DEL BOT
+// ======================
+
+global.botname = 'LisaBot'
+global.author = 'DocSebas'
+
+// ======================
+// BASE DE DATOS SIMPLE
+// ======================
+
+global.DATABASE = {
+  data: {
+    users: {},
+    chats: {},
+    banned: 0
+  }
+}
+
+// ======================
+// PLUGINS
+// ======================
+
+global.plugins = {}
