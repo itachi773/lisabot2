b17baf272b94eee526eb626df0aae70407f98336
DocSebas7 <81327636+itachi773@users.noreply.github.com>
2026-02-09 21:57:44 -0500

    Update add.js

--- plugins/add.js
+++ plugins/add.js
@@ -1,20 +1,23 @@
 let handler = async (m, { conn, args }) => {
-  let users = args.join` `.split`,`.map(v => v.replace(/\D/g, '') + '@s.whatsapp.net').filter(v => v.length > 20)
-  conn.groupAdd(m.chat, users)
-}
-handler.command = /^(add|\+)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = true
-handler.private = false
+  let users = args
+    .join(' ')
+    .split(',')
+    .map(v => v.replace(/\D/g, '') + '@s.whatsapp.net')
+    .filter(v => v.length > 20)
 
+  if (!users.length) return
+
+  await conn.groupAdd(m.chat, users)
+}
+
+handler.command = /^(add|\+)$/i
+handler.group = true
 handler.admin = true
 handler.botAdmin = true
-
 handler.fail = null
 
 module.exports = handler
+
 
 
 
