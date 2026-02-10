5d777799aa4359d6669e5f46d3d48e72e4b874dd
DocSebas7 <81327636+itachi773@users.noreply.github.com>
2026-02-09 21:58:33 -0500

    Update addprems.js

--- plugins/addprems.js
+++ plugins/addprems.js
@@ -1,19 +1,28 @@
-const { MessageType } = require('@adiwajshing/baileys')
-
-let handler = async(m, { conn, text }) => {
-let who
+let handler = async (m, { conn }) => {
+  let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw 'Etiquete a uno, e ingrese el número para verificación!'
-  // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
-    global.DATABASE._data.chats[m.chat].premium = true
-  var nomor = m.sender
-    m.reply(`*Listo usuario agregado como premium✅*\n\n*Numero : wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Expired:* 30Days\n*Thanks For Added Premium !*`)
-  
+
+  if (!who) throw 'Etiquete a un usuario para hacerlo premium'
+
+  let user = global.DATABASE.data.users[who]
+  if (!user) throw 'Usuario no encontrado en la base de datos'
+
+  user.premium = true
+  user.premiumTime = Date.now() + 30 * 24 * 60 * 60 * 1000 // 30 días
+
+  let nomor = who.split('@')[0]
+  m.reply(
+    `*Usuario agregado como Premium ✅*\n\n` +
+    `*Número:* wa.me/${nomor}\n` +
+    `*Expira:* 30 días`
+  )
 }
-handler.help = ['addprems <nomor>']
+
+handler.help = ['addprems @usuario']
 handler.tags = ['owner']
 handler.command = /^addprems$/i
 handler.rowner = true
 
 module.exports = handler
+
